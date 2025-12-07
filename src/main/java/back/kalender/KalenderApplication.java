78cd165ca72084c2cca503e067b88b810b0a3531
hznnoy <yeonii9902@gmail.com>
2025-12-07 18:44:59 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/back/kalender/KalenderApplication.java
@@ -0,0 +1,13 @@
+package back.kalender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KalenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/back/kalender/KalenderApplicationTests.java
@@ -0,0 +1,13 @@
+package back.kalender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
