96b7e72f0027ff413d9975e706782988b765cf00
BackSeungBeom <seungbeom1234@gmail.com>
2025-12-08 11:15:17 +0900

    init

---
+++ src/main/java/back/kalender/global/springDoc/SpringDoc.java
@@ -0,0 +1,52 @@
+package back.kalender.global.springDoc;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "UniMate API 서버",
+                version = "beta",
+                description = """
+                        UniMate 서버 API 문서입니다.
+                        <br>
+                        🔑 JWT 인증이 필요한 API를 테스트하려면 상단의 <b>Authorize</b> 버튼을 눌러
+                        '{토큰}' 형식으로 JWT를 입력하세요.
+                        """
+        ),
+        security = {@SecurityRequirement(name = "BearerAuth")}
+)
+@SecurityScheme(
+        name = "BearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        description = "JWT 토큰을 '{token}' 형식으로 입력하세요."
+)
+public class SpringDoc {
+
+    @Bean
+    public GroupedOpenApi groupApiV1() {
+        return GroupedOpenApi.builder()
+                .group("apiV1")
+                .pathsToMatch("/api/v1/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi groupController() {
+        return GroupedOpenApi.builder()
+                .group("home")
+                .pathsToExclude("/api/**")
+                .build();
+    }
+
+}
