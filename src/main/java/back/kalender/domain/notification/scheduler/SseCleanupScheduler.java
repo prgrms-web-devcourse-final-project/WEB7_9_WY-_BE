33147f3f0b15863b520f4e22ee592a72247dbf52
Yejin <dpwls8972@gmail.com>
2026-01-03 22:38:42 +0900

    fix(noti): SSE 연결 오류 해결

--- src/main/java/back/kalender/domain/notification/scheduler/SseCleanupScheduler.java
+++ src/main/java/back/kalender/domain/notification/scheduler/SseCleanupScheduler.java
@@ -2,10 +2,12 @@ package back.kalender.domain.notification.scheduler;
 
 import back.kalender.domain.notification.repository.EmitterRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class SseCleanupScheduler {
 
@@ -14,5 +16,11 @@ public class SseCleanupScheduler {
     @Scheduled(fixedRate = 60000)
     public void runCleanup() {
         emitterRepository.deleteExpiredEventCache();
+    }
+
+    @Scheduled(fixedRate = 45000)
+    public void sendHeartbeat() {
+        emitterRepository.sendHeartbeatToAll();
+        log.debug("SSE Heartbeat 전송 완료");
     }
 }
